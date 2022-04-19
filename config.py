f176637a3398d8df89a78f59ad76ec4fcbb633f0
avv77 <mag7777@mail.ru>
2022-04-19 21:00:46 +0300

    19042022_1

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 BOT_TOKEN = '5241979800:AAE83fw5jgW4iNJoANXb1o4vZs5WmZXUfmM'
-APP_URL = "https://anekdot-avv.herokuapp.com/" + BOT_TOKEN
-DB_URI = 'postgres://ioygtgammlrxof:' \
-         '42488573df33cc147601007091f6f0a19fc81e7617bf30f763a963e82e9281a1@ec2-63-32-248-14.eu-west-1.compute.' \
-         'amazonaws.com:5432/ddvquv67ao54lt'
+APP_URL = "https://anegdot.herokuapp.com/" + BOT_TOKEN
+DB_URI = 'postgres://kpjgpwdidqifxi:e68a164533b3e0258166c90d1d260e151cd964a2111b270be7b9814b4d11bb46@' \
+         'ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/dcek38kv49gu12'
